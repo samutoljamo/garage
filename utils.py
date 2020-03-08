4b9a18a128977bd09c5d925c1688df4aada0c6bb
Samu Toljamo <samu.toljamo@gmail.com>
2020-03-08 17:57:54 +0200

    Added missing parenthesis

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def to_bool(string):
     if string.lower() == "true":
         return True
-    elif string.lower == "false":
+    elif string.lower() == "false":
         return False
     return None
