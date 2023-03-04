32277cde8298d1db241cf5742dfa2d82a75553ce
Samu Toljamo <samu.toljamo@avanio.com>
2023-03-04 23:17:48 +0200

    Added install scripts

--- notifier/install.sh
+++ notifier/install.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
+echo "Installing notifier service"
 
 # stop the service
 systemctl stop notifier.service
+
+
+# if service file exists, ask if it should be overwritten
+if [ -f "/etc/systemd/system/notifier.service" ]; then
+    echo "Service file already exists. Overwrite? (y/n)"
+    read overwrite
+    if [ "$overwrite" = "y" ]; then
+      # ask for the user of the service
+      echo "Enter the user of the service:"
+      read user
+      sed -i -e "s/{{user}}/$user/g" notifier.service
+
+      # ask for the webhook url
+      echo "Enter the webhook url:"
+      read webhook
+      sed -i -e "s/{{discord_webhook}}/$webhook/g" notifier.service
+
+
+      # copy the service file to /etc/systemd/system
+      cp notifier.service /etc/systemd/system/notifier.service
+    fi
+fi
 
 # remove the old directory
 rm -rf /opt/garage/notifier
@@ -9,8 +32,7 @@ rm -rf /opt/garage/notifier
 # copy files to /opt/garage-bot
 cp -r * /opt/garage/notifier
 
-# copy the service file to /etc/systemd/system
-cp notifier.service /etc/systemd/system/garage.service
+
 
 systemctl daemon-reload
 systemctl enable notifier.service
